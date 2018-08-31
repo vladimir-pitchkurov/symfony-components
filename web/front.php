9eef266928588a3bfbfc6c02731093e92c718c72
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-31 12:30:10 +0300

    move front-controller and create .htaccess

--- web/front.php
+++ web/front.php
@@ -1,6 +1,6 @@
 <?php
-// framework/front.php
-require_once __DIR__.'/vendor/autoload.php';
+// example.com/web/front.php
+require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +9,15 @@ $request = Request::createFromGlobals();
 $response = new Response();
 
 $map = array(
-    '/hello' => __DIR__.'/hello.php',
-    '/bye'   => __DIR__.'/bye.php',
+    '/hello' => __DIR__.'/../src/pages/hello.php',
+    '/bye'   => __DIR__.'/../src/pages/bye.php',
 );
 
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
-    require $map[$path];
+    ob_start();
+    include $map[$path];
+    $response->setContent(ob_get_clean());
 } else {
     $response->setStatusCode(404);
     $response->setContent('Not Found');
