95d8fd9bf2521d1569d756061e63634643f21216
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-08-31 11:51:05 +0300

    add symphony components request / response

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
-<?php 
+<?php
 
-$name = isset($_GET['name']) ? $_GET['name'] : 'World';
-header('Content-Type: text/html; charset=utf-8');
+require_once __DIR__.'/vendor/autoload.php';
 
-printf('Hello %s ', $name);
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$name = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
